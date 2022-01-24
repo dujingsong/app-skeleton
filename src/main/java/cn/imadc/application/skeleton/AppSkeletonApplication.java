ba51b1872c0d10d0de585f7a1804776b8287456b
杜劲松 <farben_dujinsong@farben.com>
2022-01-24 18:35:15 +0800

    init

---
+++ src/main/java/cn/imadc/application/skeleton/AppSkeletonApplication.java
@@ -0,0 +1,13 @@
+package cn.imadc.application.skeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSkeletonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppSkeletonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/imadc/application/skeleton/AppSkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.imadc.application.skeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSkeletonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
